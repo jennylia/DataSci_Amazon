3d4cee5443f9674703fca9de78efa735cc5d64a4
Jenny Lian <jennylian100@gmail.com>
2017-04-07 23:14:44 -0700

    try to make some other text files

--- nodeCreator.py
+++ nodeCreator.py
@@ -1,14 +1,7 @@
-# f = open("amazon0302.txt",'r')
-# with open('amazon0302.txt','r') as f:
-# 	for _ in xrange(17):
-#     	next(f)
-
-#     for line in f:
-#         for word in line.split():
-#            print(word)      
-
 import sys
 import itertools
+from py2neo import *
+
 with open('amazon0302.txt') as f:
     for line in itertools.islice(f, 4, None):  # start=5, stop=None
     	for line in f:
@@ -16,4 +9,13 @@ with open('amazon0302.txt') as f:
     		if (len(arr) < 2):
     			sys.exit(0)
     		print("length of array len(arr)", len(arr))
-    		print("first element", arr[0])
+    		first_ID = arr[0]
+    		second_ID = arr[1]
+    		#Creating the nodes
+    		print("first element", first_ID)
+    		first = Node("Product", id=first_ID)
+    		print("second element", second_ID)
+    		second = Node("Product", id=second_ID)
+			graph.create(first | second)
+
+
